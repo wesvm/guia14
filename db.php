3d41abe70967df0bc90537aefae7cc0864575993
blue <samuelwalter2001@gmail.com>
2024-08-01 23:42:17 -0500

    port

--- db.php
+++ db.php
@@ -4,8 +4,9 @@ $db_host = $_ENV["DB_HOST"];
 $db_user = $_ENV["DB_USER"];
 $db_pass = $_ENV["DB_PASS"];
 $db_name = $_ENV["DB_NAME"];
+$db_port = $_ENV["DB_PORT"];
 
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
