9161d6a4ef0116ac254b7edb3d8c2a9285086a1b
blue <samuelwalter2001@gmail.com>
2024-08-01 23:38:28 -0500

    env

--- db.php
+++ db.php
@@ -1,6 +1,11 @@
 <?php
 
-$conn = new mysqli('localhost', 'root', '', 'db_movil');
+$db_host = $_ENV["DB_HOST"];
+$db_user = $_ENV["DB_USER"];
+$db_pass = $_ENV["DB_PASS"];
+$db_name = $_ENV["DB_NAME"];
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
